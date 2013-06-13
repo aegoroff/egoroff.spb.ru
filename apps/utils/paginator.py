6f76c032b27f71ecc0da3ad24105f66bc323ff89
egr <egoroff@gmail.com>
2013-06-13 13:41:37 +0400

    small datastore usage

--- apps/utils/paginator.py
+++ apps/utils/paginator.py
@@ -44,8 +44,7 @@ class Paginator(object):
         top = bottom + self.per_page
         page_items = self.object_list[bottom:top+1]
     else:
-        f = self.object_list.fetch(self.per_page, offset=bottom)
-        page_items = [item for item in f]
+        page_items = self.object_list.fetch(self.per_page, offset=bottom)
     if not page_items:
       if number == 1 and self.allow_empty_first_page:
         pass
@@ -62,9 +61,7 @@ class Paginator(object):
     try:
       if type(self.object_list) == list:
         return len(self.object_list)
-      # HACK:
-      lst = [item for item in self.object_list]
-      return len(lst)
+      return self.object_list.count()
     except (AttributeError, TypeError):
       # AttributeError if object_list has no count() method.
       # TypeError if object_list.count() requires arguments
