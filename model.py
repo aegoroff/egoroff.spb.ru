175ab9d267a532fb71ed4198b9116d97c3979170
egr <egoroff@gmail.com>
2013-12-19 12:48:51 +0400

    function set() replaced by set literal.

--- model.py
+++ model.py
@@ -8,9 +8,7 @@ from uuid import uuid4
 class Base(ndb.Model, modelx.BaseX):
   created = ndb.DateTimeProperty(auto_now_add=True)
   modified = ndb.DateTimeProperty(auto_now=True)
-  _PROPERTIES = set([
-      'key', 'id', 'created', 'modified', 'created_ago', 'modified_ago',
-    ])
+  _PROPERTIES = {'key', 'id', 'created', 'modified', 'created_ago', 'modified_ago'}
 
 
 class Config(Base, modelx.ConfigX):
@@ -22,16 +20,9 @@ class Config(Base, modelx.ConfigX):
   flask_secret_key = ndb.StringProperty(default=str(uuid4()).replace('-', ''))
   twitter_consumer_key = ndb.StringProperty(default='')
   twitter_consumer_secret = ndb.StringProperty(default='')
-  _PROPERTIES = Base._PROPERTIES.union(set([
-      'analytics_id',
-      'brand_name',
-      'facebook_app_id',
-      'facebook_app_secret',
-      'feedback_email',
-      'flask_secret_key',
-      'twitter_consumer_key',
-      'twitter_consumer_secret',
-    ]))
+  _PROPERTIES = Base._PROPERTIES.union(
+      {'analytics_id', 'brand_name', 'facebook_app_id', 'facebook_app_secret', 'feedback_email', 'flask_secret_key',
+       'twitter_consumer_key', 'twitter_consumer_secret'})
 
 
 class User(Base, modelx.UserX):
@@ -46,6 +37,4 @@ class User(Base, modelx.UserX):
   facebook_id = ndb.StringProperty(indexed=True, default='')
   twitter_id = ndb.StringProperty(indexed=True, default='')
 
-  _PROPERTIES = Base._PROPERTIES.union(set([
-      'name', 'username', 'avatar_url',
-    ]))
+  _PROPERTIES = Base._PROPERTIES.union({'name', 'username', 'avatar_url'})
