6faa325e5d2968d0c55c61c3dd0774a583e6c18d
egoroff <egoroff@gmail.com>
2014-09-01 23:41:43 +0400

    style and scripts included into build

--- main/config.py
+++ main/config.py
@@ -62,6 +62,10 @@ SCRIPTS = [
         'src/script/site/profile.coffee',
         'src/script/site/signin.coffee',
         'src/script/site/user.coffee',
+        'src/script/site/format.date.coffee',
+        'src/script/site/rotate.post.coffee',
+        'src/script/site/blog.model.coffee',
+        'src/script/site/blog.async.reader.coffee',
       ]),
   ]
   
