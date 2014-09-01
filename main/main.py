b112310c8e6970edd36c4bc6b77ef9658f39b436
egoroff <egoroff@gmail.com>
2014-09-01 23:29:59 +0400

    main code migration

--- main/main.py
+++ main/main.py
@@ -2,7 +2,13 @@
 
 import logging
 
+import sys
+from urlparse import urljoin
+import site_map
+import typographus
+
 from flask.ext import wtf
+from flask import request, current_app
 import flask
 import wtforms
 
@@ -20,12 +26,85 @@ app.jinja_env.globals.update(
     update_query_argument=util.update_query_argument,
   )
 
+app.jinja_env.add_extension('jinja2htmlcompress.SelectiveHTMLCompress')
 
 import admin
 import auth
 import model
 import task
 import user
+
+from apps.file.views import mod as file_view
+
+app.register_blueprint(file_view)
+
+from apps.file.admin.views import mod as file_admin_mod
+
+app.register_blueprint(file_admin_mod)
+
+from apps.portfolio.views import mod as portfolio_mod, create_apache_docs
+
+app.register_blueprint(portfolio_mod)
+
+from apps.news.views import mod as news_mod, create_posts_query
+
+app.register_blueprint(news_mod)
+
+from apps.news.admin.views import mod as news_admin_mod
+
+app.register_blueprint(news_admin_mod)
+
+from apps.compatibility.views import mod as compatibility_mod
+
+app.register_blueprint(compatibility_mod)
+
+from apps.api.v2.views import mod as api_mod
+
+app.register_blueprint(api_mod)
+
+
+def create_breadcrumbs(parents):
+    breadcrumbs = [('welcome', u'Главная', 'icon-home')]
+    append = lambda item: breadcrumbs.append((item[site_map.ID], item[site_map.TITLE], item[site_map.CLASS]))
+    map(append, parents)
+    return breadcrumbs
+
+
+def current_section():
+    for section in site_map.MAP:
+        index = section[site_map.ID]
+        section_uri = flask.url_for(index)
+        if section_uri in request.path:
+            return section, section_uri
+    return None, None
+
+@app.context_processor
+def inject_current_section():
+    curr, uri = current_section()
+    if curr:
+        return dict(current_id=curr[site_map.ID])
+    return dict(current_id="")
+
+
+@app.context_processor
+def inject_breadcrumbs():
+    if request.path == '/':
+        return dict(breadcrumbs=None)
+    curr, uri = current_section()
+    if curr:
+        if request.path == uri and (not request.query_string or request.query_string == ''):
+            return dict(breadcrumbs=create_breadcrumbs([]))
+        return dict(breadcrumbs=create_breadcrumbs([curr]))
+    return dict(breadcrumbs=create_breadcrumbs([]))
+
+
+
+@app.template_filter('typo')
+def typo(s):
+    return typographus.typo(s)
+
+
+app.jinja_env.filters["typo"] = typo
 
 
 if config.DEVELOPMENT:
@@ -38,7 +117,27 @@ if config.DEVELOPMENT:
 ###############################################################################
 @app.route('/')
 def welcome():
-  return flask.render_template('welcome.html', html_class='welcome')
+    limit = 5
+    posts = util.run_query(create_posts_query(), limit)
+    return flask.render_template(
+        'welcome.html',
+        html_class='welcome',
+        apache_docs=create_apache_docs(),
+        posts=posts
+    )
+
+
+@app.route('/search/')
+def search():
+    title=u'Поиск'
+    q = util.param('q')
+    if q:
+        title = u"Результаты по запросу: {0}".format(q)
+    return flask.render_template(
+        'search.html',
+        title=title,
+        html_class='search'
+    )
 
 
 ###############################################################################
@@ -46,23 +145,44 @@ def welcome():
 ###############################################################################
 @app.route('/sitemap.xml')
 def sitemap():
-  response = flask.make_response(flask.render_template(
-      'sitemap.xml',
-      host_url=flask.request.host_url[:-1],
-      lastmod=config.CURRENT_VERSION_DATE.strftime('%Y-%m-%d'),
-    ))
-  response.headers['Content-Type'] = 'application/xml'
-  return response
+    create = lambda path: urljoin(flask.request.url_root, path)
+    full_uri = lambda marker: create(flask.url_for(marker))
+
+    pages = [util.create_page(create(''), "weekly")]
+
+    for item in site_map.MAP:
+        if item[site_map.ID] == 'search': # filter search because it disabled it robots.txt
+            continue
+        p = util.create_page(full_uri(item[site_map.ID]), "weekly", '0.7')
+        pages.append(p)
+
+    apache_docs = create_apache_docs()
+
+    for d in apache_docs:
+        loc = create(urljoin(flask.url_for('portfolio.index'), '{0}.html'.format(d['doc'])))
+        p = util.create_page(loc, "yearly")
+        pages.append(p)
+
+    keys = util.run_query(create_posts_query(), None, keys_only=True)
+    for k in keys:
+        loc = create(urljoin(flask.url_for('news.index'), '{0}.html'.format(k.id())))
+        p = util.create_page(loc, "yearly")
+        pages.append(p)
+
+    sitemap_xml = flask.render_template('sitemap.html', pages=pages , mimetype='text/xml')
+    response = flask.make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
 
 
 ###############################################################################
 # Profile stuff
 ###############################################################################
 class ProfileUpdateForm(wtf.Form):
-  name = wtforms.StringField('Name',
+  name = wtforms.StringField(u'Имя',
       [wtforms.validators.required()], filters=[util.strip_filter],
     )
-  email = wtforms.StringField('Email',
+  email = wtforms.StringField(u'Электропочта',
       [wtforms.validators.optional(), wtforms.validators.email()],
       filters=[util.email_filter],
     )
