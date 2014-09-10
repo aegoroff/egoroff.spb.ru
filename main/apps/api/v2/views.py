733827cdea0fba698558c2c5a419039b1bba2f2f
egoroff <egoroff@gmail.com>
2014-09-10 22:08:25 +0400

    Images uploading moved to news admin module

--- main/apps/api/v2/views.py
+++ main/apps/api/v2/views.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
-import os
 import random
 from apps.news.views import get_posts_ids, create_posts_query
-from apps.utils import blobstore_handlers
 import config
 
 from flask import Blueprint, render_template, current_app, request
 import json
 from util import param, jsonify_model_db
 from apps.api.utils import except_wrap, ApiException
-import datetime
 from apps.news.models import Post
 import util
-import flask
-from auth import admin_required
-from apps.file.models import Folder, File
-from apps.utils.blobstore import get_uploads
 from datetime import datetime
 
 mod = Blueprint(
@@ -76,36 +69,3 @@ def get_post_json(key_id):
 @except_wrap
 def post_json():
     return get_post_json(param('id', int))
-
-
-@mod.route('/add_file/', methods=['POST'])
-@admin_required
-def add_blob_content():
-
-    response_object = {
-      'status': 'success',
-      'now': datetime.utcnow().isoformat(),
-      'filelink': None,
-    }
-
-    folder = Folder.retrieve_by_id(3001)
-    if not folder:
-        response_object['status'] = 'failure'
-        return util.jsonpify(response_object)
-    upload_files = get_uploads(flask.request, 'file')
-    if len(upload_files):
-        blob_info = upload_files[0]
-        if blob_info.size:
-            f = File.create(
-                blob_info.key(),
-                size=blob_info.size,
-                filename=os.path.basename(blob_info.filename.replace('\\','/')),
-                content_type=blob_info.content_type)
-            f.put()
-            if f.get_cached_url():
-                folder.files.append(f.key)
-                folder.put()
-                response_object['filelink'] = f.get_cached_url()
-        else:
-            blob_info.delete()
-    return util.jsonpify(response_object)
