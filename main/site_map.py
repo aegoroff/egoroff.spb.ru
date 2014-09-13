9a1eed26f6d48ec06fc361b6c62f534bc3e6288d
egoroff <egoroff@gmail.com>
2014-09-13 09:01:48 +0400

    feedback section moved to sitemap

--- main/site_map.py
+++ main/site_map.py
@@ -89,6 +89,11 @@ MAP = [
                 CLASS: "fa fa-rss",
                 TITLE: u"RSS"
             },
+            {
+                ID: "feedback",
+                CLASS: "fa fa-comment",
+                TITLE: u"Фидбек"
+            },
         ]
     },
     {
