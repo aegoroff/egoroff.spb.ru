9c1d31bf32c145102a0739c9be0503149f046394
egoroff <egoroff@gmail.com>
2014-09-26 00:07:02 +0400

    shBrushLess included

--- main/config.py
+++ main/config.py
@@ -74,6 +74,7 @@ SCRIPTS = [
         'src/script/syntax-highlighter/shBrushHashQuery.js',
         'src/script/syntax-highlighter/shBrushPython.js',
         'src/script/syntax-highlighter/shBrushCss.js',
+        'src/script/syntax-highlighter/shBrushLess.js',
         'src/script/jquery.plugins/jquery.form.js',
         'src/script/jquery.plugins/jquery.metadata.js',
         'src/script/jquery.plugins/jquery.mosaic.js',
