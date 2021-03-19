9b9b59cefb3acf03dd6d6432a4f1a7e798ca481e
egoroff <egoroff@gmail.com>
2021-03-19 19:39:55 +0300

    code duplication eliminated

--- app/blog.go
+++ app/blog.go
@@ -17,32 +17,25 @@ func NewBlog() *Blog {
 func (b *Blog) Route(r *gin.Engine) {
 	blog := r.Group("/blog")
 	{
-		blog.GET("/", func(c *gin.Context) {
-			ctx := NewContext("blog", c)
-			appContext := ctx["ctx"].(*Context)
-			s := appContext.Section("blog")
-			ctx["title"] = s.Title
-			ctx["poster"] = NewPoster(20)
-
-			c.HTML(http.StatusOK, "blog/index.html", ctx)
-		})
-
-		blog.GET("/:page/", func(c *gin.Context) {
-			ctx := NewContext("blog", c)
-			appContext := ctx["ctx"].(*Context)
-			page, _ := strconv.ParseInt(c.Param("page"), 10, 32)
-
-			title := appContext.Section("blog").Title
-			if page > 1 {
-				title = fmt.Sprintf("%d-я страница", page)
-			}
-			ctx["title"] = title
-			poster := NewPoster(20)
-
-			poster.SetPage(int(page))
-			ctx["poster"] = poster
-
-			c.HTML(http.StatusOK, "blog/index.html", ctx)
-		})
+		blog.GET("/", b.index)
+		blog.GET("/:page/", b.index)
 	}
+}
+
+func (*Blog) index(c *gin.Context) {
+	ctx := NewContext("blog", c)
+	appContext := ctx["ctx"].(*Context)
+	page, _ := strconv.ParseInt(c.Param("page"), 10, 32)
+
+	title := appContext.Section("blog").Title
+	if page > 1 {
+		title = fmt.Sprintf("%d-я страница", page)
+	}
+	ctx["title"] = title
+	poster := NewPoster(20)
+
+	poster.SetPage(int(page))
+	ctx["poster"] = poster
+
+	c.HTML(http.StatusOK, "blog/index.html", ctx)
 }
