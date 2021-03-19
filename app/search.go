094ea323bd30a9eab0d528b92ab3d5aeb9c8e095
egoroff <egoroff@gmail.com>
2021-03-19 13:48:01 +0300

    search restored

--- app/search.go
+++ app/search.go
@@ -6,6 +6,8 @@ import (
 )
 
 type Search struct {
+	Key string
+	Cx  string
 }
 
 func NewSearch() *Search {
@@ -14,8 +16,11 @@ func NewSearch() *Search {
 
 func (s *Search) Route(r *gin.Engine) {
 	r.GET("/search/", func(c *gin.Context) {
-
 		ctx := NewContext("search", c)
+		ctx["action_uri"] = "https://www.googleapis.com/customsearch/v1"
+		s.Key = ctx["ctx"].(*Context).conf.SearchApiKey
+		s.Cx = ctx["ctx"].(*Context).conf.GoogleSiteId
+		ctx["search"] = s
 
 		c.HTML(http.StatusOK, "search.html", ctx)
 	})
