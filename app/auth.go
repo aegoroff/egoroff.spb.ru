04e1d88c1059de265555782f74d400aa563bd94b
egoroff <egoroff@gmail.com>
2021-03-25 00:54:40 +0300

    callback draft

--- app/auth.go
+++ app/auth.go
@@ -6,11 +6,11 @@ import (
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/zalando/gin-oauth2/google"
+	"log"
 	"net/http"
 )
 
 type Auth struct {
-	state string
 }
 
 func NewAuth() *Auth {
@@ -20,23 +20,35 @@ func NewAuth() *Auth {
 func (a *Auth) Route(r *gin.Engine) {
 	r.GET("/login", a.signin)
 	r.GET("/signin", a.signin)
+	r.GET("/_s/callback/google/authorized/", a.callback).Use(google.Auth())
 }
 
 func (a *Auth) signin(c *gin.Context) {
 	ctx := NewContext(c)
-	a.state = randToken()
+	state := randToken()
 	session := sessions.Default(c)
-	session.Set("state", a.state)
-	session.Save()
+	session.Set("state", state)
+	err := session.Save()
+	if err != nil {
+		log.Println(err)
+	}
 
 	ctx["title"] = "Авторизация"
-	ctx["google_signin_url"] = google.GetLoginURL(a.state)
+	ctx["google_signin_url"] = google.GetLoginURL(state)
 
 	c.HTML(http.StatusOK, "signin.html", ctx)
 }
 
+func (a *Auth) callback(c *gin.Context) {
+	log.Println(c.MustGet("user").(google.User))
+	c.Redirect(http.StatusFound, "/")
+}
+
 func randToken() string {
 	b := make([]byte, 32)
-	rand.Read(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Println(err)
+	}
 	return base64.StdEncoding.EncodeToString(b)
 }
