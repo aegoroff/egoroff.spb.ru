36ce76e7d1a87595f44338c834efee345ab723d6
egoroff <egoroff@gmail.com>
2021-04-03 12:25:00 +0300

    experiments

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Navigation msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
 
 @Component({
   components: {
-    HelloWorld
+    Navigation: Navigation
   }
 })
 export default class App extends Vue {}
