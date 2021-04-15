b4bbf509bf89153ee39dab1b1b2ae42bd26adc19
egoroff <egoroff@gmail.com>
2021-04-15 12:51:26 +0300

    refactoring: query method introduced to eliminate duplication

--- app/db/repository.go
+++ app/db/repository.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"cloud.google.com/go/datastore"
+	"context"
 	"egoroff.spb.ru/app/domain"
 	"egoroff.spb.ru/app/lib"
 	"log"
@@ -101,43 +102,31 @@ func (r *Repository) NewAuth(user *domain.Auth, name string) error {
 }
 
 func (r *Repository) Auth(name string) *domain.Auth {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
-
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
 	k := datastore.NameKey("Auth", name, nil)
 	var auth domain.Auth
-	err = c.Get(ctx, k, &auth)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
+
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		err := c.Get(ctx, k, &auth)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
 	return &auth
 }
 
 func (r *Repository) AuthKeys() []string {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
+	var keys []*datastore.Key
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		q := datastore.NewQuery("Auth").KeysOnly()
+		var auths []*domain.Auth
+		k, err := c.GetAll(ctx, q, &auths)
+		if err != nil {
+			log.Println(err)
+		}
+		keys = k
+	})
 
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
-	q := datastore.NewQuery("Auth").KeysOnly()
-	var auths []*domain.Auth
-	keys, err := c.GetAll(ctx, q, &auths)
-	if err != nil {
-		log.Println(err)
-	}
 	result := make([]string, len(keys))
 	for i, k := range keys {
 		result[i] = k.Name
@@ -146,142 +135,115 @@ func (r *Repository) AuthKeys() []string {
 }
 
 func (r *Repository) Tags() []*domain.TagContainter {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
+	var containers []*domain.TagContainter
 
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		q := datastore.NewQuery("Post").Filter("is_public=", true).Project("tags", "created", "__key__")
 
-	q := datastore.NewQuery("Post").Filter("is_public=", true).Project("tags", "created", "__key__")
-	var containters []*domain.TagContainter
-	_, err = c.GetAll(ctx, q, &containters)
-	if err != nil {
-		log.Println(err)
-	}
-	return containters
+		_, err := c.GetAll(ctx, q, &containers)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	return containers
 }
 
 func (r *Repository) PostKeys() []*datastore.Key {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
+	var keys []*datastore.Key
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		q := datastore.NewQuery("Post").KeysOnly()
+		var posts []*domain.Post
+		k, err := c.GetAll(ctx, q, &posts)
+		if err != nil {
+			log.Println(err)
+		}
+		keys = k
+	})
 
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
-	q := datastore.NewQuery("Post").KeysOnly()
-	var posts []*domain.Post
-	keys, err := c.GetAll(ctx, q, &posts)
-	if err != nil {
-		log.Println(err)
-	}
 	return keys
 }
 
 func (r *Repository) Folders() []*domain.Folder {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
-
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
-	q := datastore.NewQuery("Folder").Filter("is_public=", true)
 	var folders []*domain.Folder
-	_, err = c.GetAll(ctx, q, &folders)
-	if err != nil {
-		log.Println(err)
-	}
+
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		q := datastore.NewQuery("Folder").Filter("is_public=", true)
+		_, err := c.GetAll(ctx, q, &folders)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
 	return folders
 }
 
 func (r *Repository) File(k *datastore.Key) *domain.File {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
-
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
 	var file domain.File
-	err = c.Get(ctx, k, &file)
-	if err != nil {
-		log.Println(err)
-	}
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		err := c.Get(ctx, k, &file)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
 	return &file
 }
 
 func (r *Repository) FileByUid(uid string) *domain.File {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
-
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
 	var files []*domain.File
-	q := datastore.NewQuery("File").Filter("uid=", uid)
-	_, err = c.GetAll(ctx, q, &files)
-	if err != nil || len(files) == 0 {
-		log.Println(err)
+
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		q := datastore.NewQuery("File").Filter("uid=", uid)
+		_, err := c.GetAll(ctx, q, &files)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	if len(files) == 0 {
 		return nil
 	}
+
 	return files[0]
 }
 
 func (r *Repository) Blob(uid string) *domain.Blob {
-	c, err := r.conn.Connect()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-	defer lib.Close(c)
-
-	ctx, cancel := r.conn.newContext()
-	defer cancel()
-
-	k := datastore.NameKey("_blobmigrator_BlobKeyMapping", uid, nil)
 	var blob domain.Blob
-	err = c.Get(ctx, k, &blob)
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
+
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		k := datastore.NameKey("_blobmigrator_BlobKeyMapping", uid, nil)
+		err := c.Get(ctx, k, &blob)
+		if err != nil {
+			log.Println(err)
+		}
+	})
 	return &blob
 }
 
 func (r *Repository) Post(id int64) *domain.Post {
+	var post domain.Post
+	r.query(func(c *datastore.Client, ctx context.Context) {
+		k := datastore.IDKey("Post", id, nil)
+		err := c.Get(ctx, k, &post)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	return &post
+}
+
+func (r *Repository) query(action func(c *datastore.Client, ctx context.Context)) {
 	c, err := r.conn.Connect()
 	if err != nil {
 		log.Println(err)
-		return nil
+		return
 	}
 	defer lib.Close(c)
 
 	ctx, cancel := r.conn.newContext()
 	defer cancel()
 
-	k := datastore.IDKey("Post", id, nil)
-	var post domain.Post
-	err = c.Get(ctx, k, &post)
-	if err != nil {
-		log.Println(err)
-	}
-	return &post
+	action(c, ctx)
 }
