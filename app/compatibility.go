a533898f6a2ffa9c247a2981ef7c59edc7dc2348
egoroff <egoroff@gmail.com>
2021-04-26 16:23:39 +0300

    one more migration added related to apache documents

--- app/compatibility.go
+++ app/compatibility.go
@@ -49,6 +49,11 @@ func (cm *compatibility) Route(r *gin.Engine) {
 		c.Redirect(http.StatusMovedPermanently, "/portfolio/")
 	})
 
+	r.GET("/portfolio/apache/:doc", func(c *gin.Context) {
+		doc := c.Param("doc")
+		c.Redirect(http.StatusMovedPermanently, "/portfolio/"+doc)
+	})
+
 	r.GET("/opinions/", func(c *gin.Context) {
 		c.Redirect(http.StatusMovedPermanently, "/blog/")
 	})
