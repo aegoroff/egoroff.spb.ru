fb76bd5ac3e68232220481adcfbb1ea67da332a6
egoroff <egoroff@gmail.com>
2021-12-22 07:57:46 +0300

    ui: Dashboard and Users classes decorated by @Component

--- ui/src/views/admin/Dashboard.vue
+++ ui/src/views/admin/Dashboard.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
+@Component
 export default class Dashboard extends Vue {
 }
 </script>
--- ui/src/views/admin/Users.vue
+++ ui/src/views/admin/Users.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
+@Component
 export default class Users extends Vue {
 }
 </script>
