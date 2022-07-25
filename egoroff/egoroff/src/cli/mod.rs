46201f86645607451426e848f95d2d245273d63a
Alexander Egorov <egoroff@gmail.com>
2022-07-25 20:20:16 +0300

    feat: start server implementation in rust

---
+++ egoroff/egoroff/src/cli/mod.rs
@@ -0,0 +1,9 @@
+
+pub mod server;
+pub mod version;
+
+pub const SERVER_SUBCOMMAND: &str = "server";
+pub const SERVER_DESCRIPTION: &str = "Run the server";
+
+pub const VERSION_SUBCOMMAND: &str = "version";
+pub const VERSION_DESCRIPTION: &str = "Display the version and build information";
--- egoroff/egoroff/src/main.rs
+++ egoroff/egoroff/src/main.rs
@@ -1,3 +1,21 @@
+use clap::{App, SubCommand};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .about(clap::crate_description!())
+        .subcommand(SubCommand::with_name(cli::VERSION_SUBCOMMAND).about(cli::VERSION_DESCRIPTION))
+        .subcommand(
+            SubCommand::with_name(cli::SERVER_SUBCOMMAND).about(cli::SERVER_DESCRIPTION),
+        )
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
+        .get_matches();
+
+    if let Some(_) = cli.subcommand_matches(cli::VERSION_SUBCOMMAND) {
+        cli::version::run();
+    } else if let Some(server_matches) = cli.subcommand_matches(cli::SERVER_SUBCOMMAND) {
+        cli::server::run(server_matches);
+    }
 }
