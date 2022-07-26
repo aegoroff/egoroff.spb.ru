174de35b294a42a51044b3641c1e546a40cb42b3
Alexander Egorov <egoroff@gmail.com>
2022-07-26 20:52:32 +0300

    feat: TLS support added

--- egoroff/server/src/lib.rs
+++ egoroff/server/src/lib.rs
@@ -1,6 +1,14 @@
+use axum::{
+    extract::Host,
+    response::Redirect,
+    http::{StatusCode, Uri},
+    BoxError, Router,
+    handler::Handler,
+};
+use axum_server::tls_rustls::RustlsConfig;
 use std::env;
-use std::net::SocketAddr;
-use tokio::signal;
+use std::{net::SocketAddr, path::PathBuf};
+use std::time::Duration;
 use tower::ServiceBuilder;
 use tower_http::classify::ServerErrorsFailureClass;
 use tower_http::cors::{Any, CorsLayer};
@@ -8,9 +16,12 @@ use tower_http::trace::TraceLayer;
 use tracing::Span;
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use axum::{Router, Server};
-use std::time::Duration;
 
+#[derive(Clone, Copy)]
+struct Ports {
+    http: u16,
+    https: u16,
+}
 
 pub async fn run() {
     tracing_subscriber::registry()
@@ -20,15 +31,70 @@ pub async fn run() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let port = env::var("EGOROFF_PORT").unwrap_or_else(|_| String::from("4200"));
-    let socket: SocketAddr = format!("0.0.0.0:{port}").parse().unwrap();
+    let http_port = env::var("EGOROFF_HTTP_PORT").unwrap_or_else(|_| String::from("4200"));
+    let https_port = env::var("EGOROFF_HTTPS_PORT").unwrap_or_else(|_| String::from("4201"));
+
+    let ports = Ports {
+        http: http_port.parse().unwrap(),
+        https: https_port.parse().unwrap(),
+    };
+
+    tokio::spawn(redirect_http_to_https(ports));
+
+    let socket: SocketAddr = format!("0.0.0.0:{https_port}").parse().unwrap();
     tracing::debug!("listening on {socket}");
+
+    // configure certificate and private key used by https
+    let cert_dir = env::var("EGOROFF_CERT_DIR").unwrap_or_else(|_| String::from("."));
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from(&cert_dir)
+            .join("egoroff_spb_ru.crt"),
+        PathBuf::from(&cert_dir)
+            .join("egoroff_spb_ru.key.pem"),
+    )
+    .await
+    .expect("Certificate cannot be loaded");
 
     let app = create_routes();
 
-    Server::bind(&socket)
+    axum_server::bind_rustls(socket, config)
         .serve(app.into_make_service())
-        .with_graceful_shutdown(shutdown_signal())
+        //.with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn redirect_http_to_https(ports: Ports) {
+    fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri, BoxError> {
+        let mut parts = uri.into_parts();
+
+        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+
+        if parts.path_and_query.is_none() {
+            parts.path_and_query = Some("/".parse().unwrap());
+        }
+
+        let https_host = host.replace(&ports.http.to_string(), &ports.https.to_string());
+        parts.authority = Some(https_host.parse()?);
+
+        Ok(Uri::from_parts(parts)?)
+    }
+
+    let redirect = move |Host(host): Host, uri: Uri| async move {
+        match make_https(host, uri, ports) {
+            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+            Err(error) => {
+                tracing::warn!(%error, "failed to convert URI to HTTPS");
+                Err(StatusCode::BAD_REQUEST)
+            }
+        }
+    };
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], ports.http));
+    tracing::debug!("http redirect listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(redirect.into_make_service())
         .await
         .unwrap();
 }
@@ -47,30 +113,4 @@ pub fn create_routes() -> Router {
                 .layer(CorsLayer::new().allow_origin(Any).allow_methods(Any))
                 .into_inner(),
         )
-}
-
-pub async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
-    }
-
-    println!("signal received, starting graceful shutdown");
 }
