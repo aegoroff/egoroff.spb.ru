afc15aa0234ce5b32fa011b153b3816b169ac1d9
egoroff <egoroff@gmail.com>
2022-12-05 09:02:06 +0300

    feat: all supported by typograph tags added to handle

--- egoroff/kernel/src/typograph.rs
+++ egoroff/kernel/src/typograph.rs
@@ -38,9 +38,22 @@ pub fn typograph(str: String) -> String {
                 text!("div", handler),
                 text!("span", handler),
                 text!("a", handler),
+                text!("dt", handler),
                 text!("dd", handler),
+                text!("li", handler),
+                text!("i", handler),
+                text!("b", handler),
+                text!("em", handler),
+                text!("strong", handler),
+                text!("h1", handler),
+                text!("h2", handler),
+                text!("h3", handler),
+                text!("h4", handler),
+                text!("h5", handler),
+                text!("h6", handler),
+                text!("td", handler),
+                text!("th", handler),
             ],
-            document_content_handlers: vec![],
             ..Settings::default()
         },
         output_sink,
@@ -69,7 +82,23 @@ mod tests {
     #[case("<span>a - b</span>", "<span>a&nbsp;&mdash; b</span>")]
     #[case("<a>a - b</a>", "<a>a&nbsp;&mdash; b</a>")]
     #[case("<dd>a - b</dd>", "<dd>a&nbsp;&mdash; b</dd>")]
+    #[case("<dt>a - b</dt>", "<dt>a&nbsp;&mdash; b</dt>")]
+    #[case("<li>a - b</li>", "<li>a&nbsp;&mdash; b</li>")]
+    #[case("<i>a - b</i>", "<i>a&nbsp;&mdash; b</i>")]
+    #[case("<b>a - b</b>", "<b>a&nbsp;&mdash; b</b>")]
+    #[case("<td>a - b</td>", "<td>a&nbsp;&mdash; b</td>")]
+    #[case("<th>a - b</th>", "<th>a&nbsp;&mdash; b</th>")]
+    #[case("<em>a - b</em>", "<em>a&nbsp;&mdash; b</em>")]
+    #[case("<h1>a - b</h1>", "<h1>a&nbsp;&mdash; b</h1>")]
+    #[case("<h2>a - b</h2>", "<h2>a&nbsp;&mdash; b</h2>")]
+    #[case("<h3>a - b</h3>", "<h3>a&nbsp;&mdash; b</h3>")]
+    #[case("<h4>a - b</h4>", "<h4>a&nbsp;&mdash; b</h4>")]
+    #[case("<h5>a - b</h5>", "<h5>a&nbsp;&mdash; b</h5>")]
+    #[case("<h6>a - b</h6>", "<h6>a&nbsp;&mdash; b</h6>")]
+    #[case("<strong>a - b</strong>", "<strong>a&nbsp;&mdash; b</strong>")]
     #[case("<pre>a - b</pre>", "<pre>a - b</pre>")]
+    #[case("<i>a - b</i> <b>c - d</b>", "<i>a&nbsp;&mdash; b</i> <b>c&nbsp;&mdash; d</b>")]
+    #[case("<i>a - b</i> <b>c -- d</b>", "<i>a&nbsp;&mdash; b</i> <b>c&nbsp;&mdash; d</b>")]
     fn typograph_tests(#[case] str: &str, #[case] expected: &str) {
         // arrange
 
