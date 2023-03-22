3013a5159df330e87aadb0c20d4503f588d2af0e
egoroff <egoroff@gmail.com>
2023-03-22 08:44:42 +0300

    Revert "feat: return starndard rust memory allocator"
    This reverts commit cf086e8c282f371a4cd301f04eb882ba84decf2c.

--- egoroff/egoroff/src/main.rs
+++ egoroff/egoroff/src/main.rs
@@ -3,6 +3,11 @@ use clap::{arg, ArgAction};
 
 use clap::{command, crate_name, Command};
 
+use mimalloc_rust::GlobalMiMalloc;
+
+#[global_allocator]
+static GLOBAL_MIMALLOC: GlobalMiMalloc = GlobalMiMalloc;
+
 mod cli;
 
 #[tokio::main]
