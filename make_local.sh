d0b194b8f918fe933d40799c15bc5914af8ee764
Alexander Egorov <egoroff@gmail.com>
2023-07-31 15:57:56 +0300

    chore: downloads design changed

--- make_local.sh
+++ make_local.sh
@@ -3,6 +3,10 @@ then
     rm -r ./home/
 fi
 
+(cd ./ui/; npm run build)
+
 mkdir ./home/
 cp -v -R ./static/ ./home/static/
-cp -v -R ./apache/ ./home/apache/ 
+cp -v -R ./apache/ ./home/apache/
+
+(cd ./egoroff/; cargo clean; cargo b --workspace; ./target/debug/egoroff server)
