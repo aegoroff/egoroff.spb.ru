c21add7df6aa458f4f19f9899b00a92c069bf3dd
Alexander Egorov <egoroff@gmail.com>
2023-09-21 19:51:48 +0300

    chore: shell scripts formatting changed

--- make_local.sh
+++ make_local.sh
@@ -4,22 +4,22 @@ base_path=./home
 [[ -d "$base_path" ]] && rm -r "$base_path"
 
 (
-	cd ./ui/
-	npm run build
+  cd ./ui/
+  npm run build
 )
 
 mkdir "$base_path"
 LOCALS=(
-	"static"
-	"apache"
+  "static"
+  "apache"
 )
 for local in "${LOCALS[@]}"; do
-	cp -v -R "./$local/" "$base_path/$local/"
+  cp -v -R "./$local/" "$base_path/$local/"
 done
 
 (
-	cd ./egoroff/
-	cargo clean
-	cargo b --workspace
-	./target/debug/egoroff server
+  cd ./egoroff/
+  cargo clean
+  cargo b --workspace
+  ./target/debug/egoroff server
 )
