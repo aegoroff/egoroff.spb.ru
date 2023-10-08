fee5ea26df662ca4d62271171bb0069c1bb83a01
egoroff <egoroff@gmail.com>
2023-10-08 08:31:09 +0300

    perf: dont serialize description property if empty

--- egoroff/kernel/src/graph.rs
+++ egoroff/kernel/src/graph.rs
@@ -12,6 +12,7 @@ pub struct SiteSection {
     pub id: String,
     pub icon: String,
     pub title: String,
+    #[serde(skip_serializing_if = "String::is_empty")]
     pub descr: String,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub keywords: Option<String>,
