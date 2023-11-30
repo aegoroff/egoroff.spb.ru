42c3c807dd601ac039e54acfea354ff8634dbb39
egoroff <egoroff@gmail.com>
2023-11-30 21:18:34 +0300

    chore: logging message fixed

--- egoroff/server/src/lib.rs
+++ egoroff/server/src/lib.rs
@@ -151,7 +151,7 @@ async fn http_server(ports: Ports, app: Router) {
                     match result {
                         Ok(r) => r,
                         Err(e) => {
-                            tracing::error!("failed to serve connection: {e:#}");
+                            tracing::error!("failed to accept connection: {e:#}");
                             continue;
                         },
                     }
