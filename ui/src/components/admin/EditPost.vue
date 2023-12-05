f3d808bb00b10f3450345b7362e84dda8388a66a
Alexander Egorov <egoroff@gmail.com>
2023-12-05 21:22:41 +0300

    feat: edit main post text implemented

--- ui/src/components/admin/EditPost.vue
+++ ui/src/components/admin/EditPost.vue
@@ -1,37 +1,44 @@
 <template>
   <b-modal size="xl" scrollable id="edit-post" :title="post.Title" @ok="onOk">
     <form ref="form">
-      <b-form-group label="Название" label-for="post-title-input" invalid-feedback="название обязательно">
-        <b-form-input
-          id="post-title-input"
-          v-model="post.Title"
-          required
-        ></b-form-input>
-      </b-form-group>
+      <b-tabs>
+        <b-tab title="Свойства" active>
+          <b-form-group label="Название" label-for="post-title-input" invalid-feedback="название обязательно">
+            <b-form-input
+              id="post-title-input"
+              v-model="post.Title"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-      <b-form-group label="Теги" label-for="post-tags">
-        <b-form-tags
-          input-id="post-tags"
-          v-model="post.Tags"
-          separator=" ,;"
-          no-add-on-enter
-        ></b-form-tags>
-      </b-form-group>
-      <b-form-group label="Краткое описание" label-for="post-short-text">
-        <b-form-textarea
-          id="post-short-text"
-          v-model="post.ShortText"
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
-      </b-form-group>
+          <b-form-group label="Теги" label-for="post-tags">
+            <b-form-tags
+              input-id="post-tags"
+              v-model="post.Tags"
+              separator=" ,;"
+              no-add-on-enter
+            ></b-form-tags>
+          </b-form-group>
+          <b-form-group label="Краткое описание" label-for="post-short-text">
+            <b-form-textarea
+              id="post-short-text"
+              v-model="post.ShortText"
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+          </b-form-group>
 
-      <b-form-checkbox id="post-public" v-model="post.IsPublic">
-        Опубликовано
-      </b-form-checkbox>
-      <b-form-checkbox id="post-markdown" v-model="post.Markdown">
-        Markdown
-      </b-form-checkbox>
+          <b-form-checkbox id="post-public" v-model="post.IsPublic">
+            Опубликовано
+          </b-form-checkbox>
+          <b-form-checkbox id="post-markdown" v-model="post.Markdown">
+            Markdown
+          </b-form-checkbox>
+        </b-tab>
+        <b-tab title="Основной текст">
+          <b-form-textarea id="post-text" v-model="post.Text" rows="20"></b-form-textarea>
+        </b-tab>
+      </b-tabs>
     </form>
   </b-modal>
 </template>
