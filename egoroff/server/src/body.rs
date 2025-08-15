5b96e0f2f9c5c890c1168a0137ad219b4e2f363b
Alexander Egorov <egoroff@gmail.com>
2025-08-15 09:46:36 +0300

    chore: unused Html struct code removed from body module

--- egoroff/server/src/body.rs
+++ egoroff/server/src/body.rs
@@ -47,40 +47,6 @@ impl<T> From<T> for Xml<T> {
     }
 }
 
-/// An HTML response.
-///
-/// Will automatically get `Content-Type: text/html`.
-#[derive(Clone, Copy, Debug)]
-pub struct Html<T>(pub T);
-
-impl<T> IntoResponse for Html<T>
-where
-    T: Into<Body>,
-{
-    fn into_response(self) -> Response {
-        (
-            [
-                (
-                    header::CONTENT_TYPE,
-                    HeaderValue::from_static("text/html; charset=utf-8"),
-                ),
-                (
-                    header::X_XSS_PROTECTION,
-                    HeaderValue::from_static("1; mode=block"),
-                ),
-            ],
-            self.0.into(),
-        )
-            .into_response()
-    }
-}
-
-impl<T> From<T> for Html<T> {
-    fn from(inner: T) -> Self {
-        Self(inner)
-    }
-}
-
 /// An Binary response.
 ///
 /// Will automatically get content type using file path.
