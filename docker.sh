dcd1db1afe1d2b8d9f71cc40d1ff7d66c2ebb4c5
Alexander Egorov <egoroff@gmail.com>
2025-08-16 08:23:14 +0300

    feat: arm docker image builiding added

--- docker.sh
+++ docker.sh
@@ -3,3 +3,5 @@
 tag="ghcr.io/aegoroff/egoroff.spb.ru:master"
 DOCKER_BUILDKIT=1 docker build . -t $tag --progress plain
 docker push $tag
+DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${tag}-arm64" --progress plain --platform=linux/arm64
+docker push "${tag}-arm64"
