29ca68eddd8e59a0070ca0ddc60633017c22f0bf
Alexander Egorov <egoroff@gmail.com>
2025-08-20 16:29:26 +0300

    feat: publish images in private registry

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tag="ghcr.io/aegoroff/egoroff.spb.ru:master"
+tag="registry.egoroff.spb.ru/egoroff/egoroff.spb.ru:master"
 DOCKER_BUILDKIT=1 docker build . -t $tag --progress plain
 docker push $tag
 DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${tag}-arm64" --progress plain --platform=linux/arm64
