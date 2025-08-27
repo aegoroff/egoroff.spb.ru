21d024c75a4883f0059389949dca6a4bf40cae02
Alexander Egorov <egoroff@gmail.com>
2025-08-27 19:46:03 +0300

    fix: purging manifest added

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@ TAG=master
 full_tag="registry.egoroff.spb.ru/egoroff/egoroff.spb.ru:${TAG}"
 DOCKER_BUILDKIT=1 docker build . -t "${full_tag}-x64"
 DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${full_tag}-arm64" --platform=linux/arm64
-docker manifest create $full_tag ${full_tag}-x64 ${full_tag}-arm64
+docker manifest create $full_tag --amend ${full_tag}-x64 --amend ${full_tag}-arm64
 docker push "${full_tag}-x64"
 docker push "${full_tag}-arm64"
-docker manifest push $full_tag
+docker manifest push --purge $full_tag
