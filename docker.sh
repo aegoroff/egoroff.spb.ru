e425833ea8709368b4b6781a9fe58c6b15863552
Alexander Egorov <egoroff@gmail.com>
2025-08-28 00:42:01 +0300

    build: progress=plain option set to command that builds arm64 image

--- docker.sh
+++ docker.sh
@@ -3,7 +3,7 @@
 TAG=master
 full_tag="registry.egoroff.spb.ru/egoroff/egoroff.spb.ru:${TAG}"
 DOCKER_BUILDKIT=1 docker build . -t "${full_tag}-x64"
-DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${full_tag}-arm64" --platform=linux/arm64
+DOCKER_BUILDKIT=1 docker build . -f DockerfileArm64 -t "${full_tag}-arm64" --platform=linux/arm64 --progress=plain
 docker manifest create $full_tag --amend ${full_tag}-x64 --amend ${full_tag}-arm64
 docker push "${full_tag}-x64"
 docker push "${full_tag}-arm64"
