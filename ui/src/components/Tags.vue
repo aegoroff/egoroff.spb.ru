2491ca8d4f00fa52aa47f4448ddefb936c3ebb77
Alexander Egorov <egoroff@gmail.com>
2025-12-14 18:07:54 +0300

    fix: tags styles restored

--- ui/src/components/Tags.vue
+++ ui/src/components/Tags.vue
@@ -87,55 +87,78 @@ export default defineComponent({
   margin: 10px auto 30px;
   padding: 20px;
   line-height: 1.6em;
-  
-  .tagRank1 {
-    font-size: 1.4em;
+
+  span {
     font-weight: bold;
+    font-style: italic;
   }
-  .tagRank2 {
-    font-size: 1.3em;
-    font-weight: bold;
+
+  a, span {
+    position: relative;
+    top: 0;
+    left: -10px;
+    z-index: 10;
+    margin: 0;
+    padding: 0 5px;
+    color: #000;
+    font-size: 1em;
+    text-decoration: none;
+  }
+
+  a.tagRank1, span.tagRank1 {
+    font-size: 2.8em;
+  }
+
+  a.tagRank2, span.tagRank2 {
+    font-size: 2.6em;
     color: #222;
   }
-  .tagRank3 {
-    font-size: 1.2em;
-    font-weight: bold;
+
+  a.tagRank3, span.tagRank3 {
+    font-size: 2.4em;
     color: #333;
   }
-  .tagRank4 {
-    font-size: 1.1em;
+
+  a.tagRank4, span.tagRank4 {
+    font-size: 2.2em;
     color: #444;
   }
-  .tagRank5 {
-    font-size: 1em;
+
+  a.tagRank5, span.tagRank5 {
+    font-size: 2em;
     color: #555;
   }
-  .tagRank6 {
-    font-size: 0.95em;
+
+  a.tagRank6, span.tagRank6 {
+    font-size: 1.8em;
     color: #666;
   }
-  .tagRank7 {
-    font-size: 0.9em;
+
+  a.tagRank7, span.tagRank7 {
+    font-size: 1.6em;
     color: #777;
   }
-  .tagRank8 {
-    font-size: 0.85em;
+
+  a.tagRank8, span.tagRank8 {
+    font-size: 1.4em;
     color: #888;
   }
-  .tagRank9 {
-    font-size: 0.8em;
+
+  a.tagRank9, span.tagRank9 {
+    font-size: 1.2em;
     color: #999;
   }
-  .tagRank10 {
-    font-size: 0.75em;
-    color: #aaa;
+
+  a.tagRank10, span.tagRank10 {
+    font-size: 1em;
+    color: #AAA;
   }
-  
+
   a:hover {
     text-decoration: underline;
     position: relative;
     z-index: 50;
-    color: #f00 !important;
+    color: #F00;
   }
 }
 </style>
