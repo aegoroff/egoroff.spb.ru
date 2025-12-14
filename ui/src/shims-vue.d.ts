7c6aa9ee4319c6a06a9f2b03f5f461a4f873d597
Alexander Egorov <egoroff@gmail.com>
2025-12-14 23:00:48 +0300

    fix: footer fixed

--- ui/src/shims-vue.d.ts
+++ ui/src/shims-vue.d.ts
@@ -3,8 +3,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
-declare module 'vue3-highlightjs' {
-  import { Plugin } from 'vue'
-  export const Vue3Highlightjs: Plugin
-}
