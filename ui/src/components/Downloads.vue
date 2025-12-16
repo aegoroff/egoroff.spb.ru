63eed8ba742cde64e4defdaa9f48cff038a0bcf6
Alexander Egorov <egoroff@gmail.com>
2025-12-16 23:43:22 +0300

    refactor: potential crash in formatBytes  fixed

--- ui/src/components/Downloads.vue
+++ ui/src/components/Downloads.vue
@@ -54,7 +54,7 @@ export default defineComponent({
     const formatBytes = (bytes: number): string => {
       if (bytes === 0) return '0 Bytes'
       const k = 1024
-      const sizes = ['Bytes', 'KB', 'MB', 'GB']
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB']
       const i = Math.floor(Math.log(bytes) / Math.log(k))
       return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i]
     }
